37db583ed2d35a5838df2413af1ac63492ccc2b5
jtribo <Ext.Javier.Tribo@cevalogistics.com>
2023-11-17 03:00:57 +0100

    SECOND COMMIT

--- src/app/components/exercice/exercice.component.ts
+++ src/app/components/exercice/exercice.component.ts
@@ -38,6 +38,10 @@ export class ExerciceComponent implements OnDestroy {
   // FIRST COMMIT
   // *************************
 
+    // *************************
+  // SECOND COMMIT
+  // *************************
+
   // *******************
   // exercise code
   // *******************
